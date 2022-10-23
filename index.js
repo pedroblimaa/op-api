74babe66df1acdab456bd98bf9964756092358d0
Pedro <pedroblimaa@gmail.com>
2022-10-23 14:10:44 -0300

    pushing repo

---
+++ index.js
@@ -0,0 +1,25 @@
+const express = require('express')
+const cors = require('cors')
+const app = express()
+const port = 3000
+
+app.use(cors())
+
+const akumaNoMi = require('./db/akuma_no_mi.json')
+const characters = require('./db/characters.json')
+
+const mainResponse = {
+    "message": "Welcome to the API",
+    "endpoints": [
+        "/characters",
+        "/akumaNoMi"
+    ]
+}
+
+app.get('/', (req, res) => res.send(mainResponse))
+
+app.get('/characters', (req, res) => res.send(characters))
+
+app.get('/akuma-no-mi', (req, res) => res.send(akumaNoMi))
+
+app.listen(port, () => console.log(`Example app listening on port ${port}!`))
